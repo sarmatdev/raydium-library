13b5b8a36350014e5d478275d63787664177e65c
Sarmat <skhachirov12@gmail.com>
2024-06-25 13:45:35 +0300

    clone

--- libraries/src/amm/openbook.rs
+++ libraries/src/amm/openbook.rs
@@ -24,7 +24,7 @@ use std::{
     thread, time,
 };
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct MarketPubkeys {
     pub market: Box<Pubkey>,
     pub req_q: Box<Pubkey>,
